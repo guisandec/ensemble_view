8b0baad1b664bf6fdf98432c7349efd859a0381c
CE Guisande Donadio <guisandec@gmail.com>
2020-04-24 19:01:54 -0300

    Mayor change: ui.py

--- ensemble_viewer.py
+++ ensemble_viewer.py
@@ -1,22 +1,43 @@
+#imporamos funciones necesaias
 from tkinter import *
 from tkinter import ttk
-from functools import partial
-from load_ensemble_data import load_ensemble_data
+#from functools import partial
 from pprint import pprint as pp
-window = Tk()
-window.title("Visualizador de ensembles")
-window.geometry("800x600")
-#window.resizable(False,False)
+from ast import literal_eval
 
-verbose = True
+# Estos son modulos desarollados por mi
+from load_ensemble_data import load_ensemble_data
+from ui import *
+
 
 def d_print(string):
     if verbose == True:
         print (string)
 
-tab_parent = ttk.Notebook(window)
+
+
+def create_new_protein_tab(tab_parent,uniprot_id):
+    tabs_dict[uniprot_id] = ttk.Frame(tab_parent)
+    tab_parent.add(tabs_dict[uniprot_id],text=uniprot_id)
+    tabs_abiertas.append(uniprot_id)
+    secuencia = ensemble_data[uniprot_id]["Sequence"]
+    create_scrolable_aa_list(tabs_dict[uniprot_id],secuencia,xx=10,yy=10,alto=50,ancho=780)
+    homologos = literal_eval(ensemble_data[uniprot_id]["PDB_CHAIN_LIST"])
+    print (homologos)
+    print (type(homologos))
+    create_listbox(tabs_dict[uniprot_id],homologos,xx=20,yy=300,alto=400,ancho=100)
+
+
+# create standar windown
+verbose = True
+window = Tk()
+window.title("Visualizador de ensembles")
+window.geometry("800x600")
 tabs_abiertas= []
+
+#un diccionario para acceder by uniprot_id
 tabs_dict = dict()
+tab_parent = ttk.Notebook(window)
 tab_selector = ttk.Frame(tab_parent)
 tab_parent.add(tab_selector,text="Select protein")
 tab_parent.pack(expand=1,fill="both")
@@ -26,113 +47,20 @@ tab_parent.pack(expand=1,fill="both")
 ensemble_data = load_ensemble_data()
 
 # Carga un dicionario con laa info para el listbox.
-protein_dict = dict()
-protein_listbox = Listbox(tab_selector)
-for index,uniprot_id in enumerate(ensemble_data.keys()):
-    protein_dict[index] = [uniprot_id,ensemble_data[uniprot_id]["Sequence"]]
-    protein_listbox.insert(index,uniprot_id)
 
-protein_sumary_label = Label(tab_selector,text="Click to show info",bg="#aaffaa")
+protein_list = list(ensemble_data.keys())
 
-def onselect(evt):
-    # Note here that Tkinter passes an event object to onselect()
-    a = protein_listbox.curselection()
-    uniprot_id = protein_dict[a[0]][0]
-    message = ""
-    pp(ensemble_data[uniprot_id])
-    for labels in ensemble_data[uniprot_id].keys():
-
-        #message += labels + ": " + ensemble_data[uniprot_id][labels] +"\n"
-        message += labels + ": " + " N/A " +"\n"
-
-    protein_sumary_label.configure(text=message)
+container1,scrollbar1,listbox1,listbox_dict1 = create_listbox(tab_selector,protein_list,xx=10,yy=10,ancho=200,alto=580)
 
 
+#Define el evento de selecionar
+def event_protein_select(evt):
+    selected_index = evt.widget.curselection()[0]
+    uniprot_id = listbox_dict1[selected_index]
+    if not uniprot_id in tabs_dict:
+        create_new_protein_tab(tab_parent,uniprot_id)
+listbox1.bind('<<ListboxSelect>>',event_protein_select)
 
-
-
-protein_listbox.bind('<<ListboxSelect>>', onselect)
-#crea una lista
-
-protein_listbox.place(x=10,y=10)
-
-protein_sumary_label.place(x=250,y=40)
-
-container_dict = dict()
-canvas_dict = dict()
-scrollbar_dict =dict()
-scrollable_frame_dict =dict()
-buton_dict = dict()
-residue_info_dict =dict()
-res_labels_dict = dict()
-
-def click_aa(uniprot_id,index,protein_seq):
-    message = "Pos: " + str(index) + "\n "
-    message += "Aminoacido: "+protein_seq[index]+ "\n "
-    message += "Propiedad 1: "+str( (index+1)**5+index )
-    residue_info_dict[uniprot_id].configure(text=message)
-    for but_id in buton_dict[uniprot_id]:
-        if str(index) == but_id:
-            buton_dict[uniprot_id][but_id].configure(background="#eeee11")
-        else:
-            buton_dict[uniprot_id][but_id].configure(background="#ffaaaa")
-
-
-def create_protein_tab(uniprot_id,protein_seq):
-    if not uniprot_id in tabs_abiertas:
-        print ("1")
-        tabs_dict[uniprot_id] = ttk.Frame(tab_parent)
-        tab_parent.add(tabs_dict[uniprot_id],text=uniprot_id)
-        tabs_abiertas.append(uniprot_id)
-
-        print ("2")
-        container_dict[uniprot_id] = ttk.Frame(tabs_dict[uniprot_id],height=50)
-        canvas_dict[uniprot_id] = Canvas(container_dict[uniprot_id],bg="#aaaaff",height=50,width=780)
-        scrollbar_dict[uniprot_id] = ttk.Scrollbar(container_dict[uniprot_id], orient="horizontal", command=canvas_dict[uniprot_id].xview)
-        scrollable_frame_dict[uniprot_id] = ttk.Frame(canvas_dict[uniprot_id],height=50)
-
-        scrollable_frame_dict[uniprot_id].bind(
-            "<Configure>",
-            lambda e: canvas_dict[uniprot_id].configure(
-                scrollregion=canvas_dict[uniprot_id].bbox("all")
-            )
-        )
-
-        canvas_dict[uniprot_id].create_window((0, 0), window=scrollable_frame_dict[uniprot_id], anchor="nw")
-        print ("3")
-        canvas_dict[uniprot_id].configure(xscrollcommand=scrollbar_dict[uniprot_id].set)
-
-
-        print ("4")
-        buton_dict[uniprot_id] = dict()
-        res_labels_dict[uniprot_id] = dict()
-        for i,aa in enumerate(protein_seq):
-            print (">"+str(i))
-            buton_dict[uniprot_id][str(i)] = Button(scrollable_frame_dict[uniprot_id],activebackground="#ffffaa",background ="#ffaaaa",text=aa,font=("Liberation Mono",8),width=1,height=1,padx=4,pady=0,command= partial(click_aa,uniprot_id,i,protein_seq))
-            buton_dict[uniprot_id][str(i)].grid(row=0,column=i)
-            res_labels_dict[uniprot_id][str(i)] = Label(scrollable_frame_dict[uniprot_id],text=str(i),font=("Liberation Mono",8))
-            res_labels_dict[uniprot_id][str(i)].grid(row=1,column=i)
-        	#buton_dict[str(i)].place(x=5+20*i,y=4)
-        residue_info_dict[uniprot_id] = Label(tabs_dict[uniprot_id],text="Click to show info",bg="#aaffaa")
-        residue_info_dict[uniprot_id].place(x=400,y=150)
-        container_dict[uniprot_id].place(x=10,y=10)
-        scrollbar_dict[uniprot_id].pack(side="bottom", fill="x")
-        canvas_dict[uniprot_id].pack(side="left", fill="y", expand=True)
-
-        d_print("5/END")
-
-
-def seleccionar():
-    a = protein_listbox.curselection()
-    print (a,protein_dict[a[0]])
-    uniprot_id = protein_dict[a[0]][0]
-    protein_seq= protein_dict[a[0]][1]
-    d_print ("Creando TAB")
-    create_protein_tab(uniprot_id,protein_seq)
-    d_print ("Tab creada")
-
-select_button = Button(tab_selector,text="Select",command=seleccionar)
-select_button.place(x=500,y=100,)
 
 
 window.mainloop()
---
+++ ui.py
@@ -0,0 +1,132 @@
+from tkinter import *
+from tkinter import ttk
+from functools import partial
+from pprint import pprint as pp
+
+
+def is_none(variable):
+    if type(variable)==type(None):
+        return True
+    else:
+        return False
+
+def create_scrolable_vertical(root,lista=[],xx=0,yy=0,ancho=200,alto=300):
+    container = ttk.Frame(root)
+    canvas = Canvas(container,width=ancho,height=alto)
+    scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
+    scrollable_frame = ttk.Frame(canvas)
+
+    scrollable_frame.bind(
+        "<Configure>",
+        lambda e: canvas.configure(
+            scrollregion=canvas.bbox("all")
+        )
+    )
+
+    canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+    for index,items in enumerate(lista):
+        ttk.Label(scrollable_frame, text=str(index) + " Sample scrolling label").pack()
+
+    container.place(x=xx,y=yy)
+    canvas.pack(side="left", fill="both", expand=True)
+    scrollbar.pack(side="right", fill="y")
+
+    return [container,canvas,scrollbar,scrollable_frame]
+
+
+#esto crea la lista de aminoacidos
+def create_scrolable_aa_list(root,lista=[],xx=10,yy=10,ancho=5800,alto=200):
+    container = ttk.Frame(root)
+    canvas = Canvas(container,width=ancho,height=alto,bg="#aaaaff")
+    scrollbar = ttk.Scrollbar(container, orient="horizontal", command=canvas.xview)
+    scrollable_frame = ttk.Frame(canvas)
+
+    scrollable_frame.bind(
+        "<Configure>",
+        lambda e: canvas.configure(
+            scrollregion=canvas.bbox("all")
+        )
+    )
+
+    canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+
+
+    def click_aa(index):
+        print ("clicked aa",index)
+        pass
+
+
+    for index,items in enumerate(lista):
+        x = Button(scrollable_frame, text=items,padx=5,pady=3,font=('Liberatarion Mono',9),command=partial(click_aa,index))
+        x.pack(side="left")
+
+
+    container.place(x=xx,y=yy)
+    canvas.pack(side="top", fill="both", expand=True)
+    scrollbar.pack(side="bottom", fill="x" )
+
+    return [container,canvas,scrollbar,scrollable_frame]
+
+
+
+
+def onselect(evt):
+    print ("Hi",evt)
+    #print (dir(evt))
+    pp(dir(evt.widget))
+
+def create_listbox(root,list = ["Example 1","Example2"],xx=0,yy=0,ancho=200,alto=300):
+    container = ttk.Frame(root)
+    sample_listbox = Listbox(container)
+    listbox_dict = dict()
+    for index,element in enumerate(list):
+        #protein_dict[index] = [uniprot_id,ensemble_data[uniprot_id]["Sequence"]]
+        listbox_dict[index] = element
+        sample_listbox.insert(index,element)
+    scrollable_frame =""# ttk.Frame(sample_listbox)
+    #canvas = Canvas(container,width=ancho,height=alto)
+    scrollbar = ttk.Scrollbar(container, orient="vertical", command=sample_listbox.yview)
+
+    """
+    scrollable_frame.bind(
+        "<Configure>",
+        lambda e:  sample_listbox.configure(
+            scrollregion=sample_listbox.bbox("all")
+        )
+    )
+    """
+    #canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+    sample_listbox.configure(yscrollcommand=scrollbar.set)
+
+
+    scrollbar.pack(side="right", fill="y")
+    sample_listbox.pack(fill="y")
+    container.place(x=xx,y=yy)
+
+    return container,scrollbar,sample_listbox,listbox_dict
+
+
+
+
+
+
+
+
+#https://blog.tecladocode.com/tkinter-scrollable-frames/
+#https://stackoverflow.com/questions/45122244/having-frames-next-to-each-other-in-tkinter
+"""
+root = Tk()
+root.title("Root papa")
+root.configure(bg="grey80")
+root.geometry("800x600")
+
+vertical_scroll = create_scrolable_vertical(root,list=list(range(0,50)),xx=10,yy=10)
+
+vertical_scroll2 = create_listbox(root,list=list(range(0,25)),xx=250,yy=10)
+
+root.mainloop()
+"""
